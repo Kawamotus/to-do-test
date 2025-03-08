44a366f1675d213a21d6258d1b07b9142c9a1e3a
Kawamotus <gustavo.kawamoto@hotmail.com>
2025-03-08 00:07:20 -0300

    chore: add dependencies and app and server

---
+++ src/app.ts
@@ -0,0 +1,43 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import authRoutes from './modules/auth/auth.routes';
+import userRoutes from './modules/user/user.routes';
+//import taskRoutes from "./modules/task/task.routes"
+import { errorHandler } from './core/http/errorHandler';
+
+class App {
+  public express: express.Application;
+
+  constructor() {
+    this.express = express();
+    this.middlewares();
+    this.database();
+    this.routes();
+    this.errorHandling();
+  }
+
+  private middlewares(): void {
+    this.express.use(express.json());
+  }
+
+  private async database(): Promise<void> {
+    try {
+      await mongoose.connect('mongodb://localhost:27017/teste-todo');
+      console.log('Conectado ao MongoDB');
+    } catch (error) {
+      console.error('Falha na conexão com MongoDB:', error);
+      process.exit(1);
+    }
+  }
+
+  private routes(): void {
+    this.express.use(authRoutes);
+    this.express.use(userRoutes);
+  }
+
+  private errorHandling(): void {
+    this.express.use(errorHandler);
+  }
+}
+
+export default new App().express;
